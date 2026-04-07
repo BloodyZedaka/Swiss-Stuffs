078a53d8acada48dbc2d82e72cf6415bf6bec0d2
BloodyZedaka <zedakapro@gmail.com>
2026-04-07 22:26:02 +0200

    Tooltip method changed

--- src/main/java/net/zedaka/swiss_stuffs/item/ModItems.java
+++ src/main/java/net/zedaka/swiss_stuffs/item/ModItems.java
@@ -1,6 +1,5 @@
 package net.zedaka.swiss_stuffs.item;
 
-import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
@@ -12,6 +11,9 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.zedaka.swiss_stuffs.Swiss_Stuffs;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -20,10 +22,12 @@ public class ModItems {
     public static final RegistryObject<Item> SWISS_FLAG = ITEMS.register("swiss_flag",
             () -> new Item(new Item.Properties()) {
                 @Override
-                public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
-                    tooltip.add(Component.translatable("item.swiss_stuffs.swiss_flag.tooltip").withStyle(ChatFormatting.GRAY));
+                public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+                    pTooltipComponents.add(Component.translatable("tooltip.swiss_stuffs.swiss_flag.tooltip").withStyle(ChatFormatting.GRAY));
+                    super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
                 }
-            });
+            }
+    );
 
     public static final RegistryObject<Item> SWISS_ARMY_KNIFE = ITEMS.register("swiss_army_knife",
             () -> new Item(new Item.Properties()));
